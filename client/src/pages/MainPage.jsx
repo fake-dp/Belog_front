d2569d53e4452ef8195c79ac5e29413d34e8c512
fake-dp <fakecoko92@gmail.com>
2023-03-29 02:45:28 +0900

    main list 위로가기 버튼 추가

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -7,6 +7,8 @@ import MainPostList from "../components/mainpage/MainPostList";
 import { useRecoilState } from "recoil";
 import { PostListState } from "../recoil/atom";
 import { LayoutMargin } from "../styles/common/LayoutMarginStyled";
+import { AiOutlineArrowUp } from "react-icons/ai";
+
 const MainPage = () => {
   const [postList, setPostList] = useRecoilState(PostListState);
   const [isLoding, setIsLoding] = useState(true);
@@ -25,10 +27,50 @@ const MainPage = () => {
     getPostList();
   }, []);
 
+  const [isScroll, setIsScroll] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY > 100) {
+      setIsScroll(true);
+    } else {
+      setIsScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const ScrollToTop = styled.button`
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #f5f5f5;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    display: ${isScroll ? "block" : "none"};
+    svg {
+      font-size: 30px;
+      color: #000;
+    }
+  `;
+
   return (
     <LayoutMargin>
       <MainHeader />
       <MainPostList isLoding={isLoding} />
+      <ScrollToTop onClick={onClick}>
+        <AiOutlineArrowUp />
+      </ScrollToTop>
     </LayoutMargin>
   );
 };
