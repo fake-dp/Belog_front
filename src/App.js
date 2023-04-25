034f131e9a8ec50609ab70a0b69e83320981aa02
fake-dp <fakecoko92@gmail.com>
2023-04-26 07:59:16 +0900

    배포 test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import AppRouter from "./router/AppRouter";
 import { Reset } from "styled-reset";
 function App() {
   console.log(
-    "%c DEV Log 포르젝트입니다.",
+    "%c DEV Log 포르젝트.",
     "color:white; font-size:20px; background: black"
   );
   console.log(
-    "%c 1.1.1 버전입니다.",
+    "%c 1.1.2 버전입니다.",
     "color:white; font-size:20px; background: black"
   );
 
